fc21bb22792519a83a4eb5ac21fde59b540960c4
Samuel Klett Navarro <samuelklettnavarro@gmail.com>
2025-05-05 17:28:15 +1000

    working modules and data split

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,7 +3,9 @@ import pandas as pd
 import pickle
 import os
 
-from sklearn.model_selection import train_test_split
+# Change to models directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 import matplotlib.pyplot as plt
 from LearningAlgorithms import ClassificationAlgorithms
 import seaborn as sns
@@ -38,6 +40,30 @@ print(f"Loaded feature data with shape: {df_features.shape}")
 # --------------------------------------------------------------
 # Create a training and test set
 # --------------------------------------------------------------
+
+subset_features = df_features.drop(columns=["participant", "exercise_category", "set"])
+
+X = subset_features.drop(columns=["exercise_name"])
+y = subset_features["exercise_name"]
+
+# stratify ensure equal distribution of exercises in the train-test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42, stratify=y
+)
+
+# Visualize the distribution of exercises in the train-test set
+fig, ax = plt.subplots(figsize=(10, 5))
+subset_features["exercise_name"].value_counts().plot(
+    kind="bar", ax=ax, color="lightblue", label="Total"
+)
+y_train.value_counts().plot(
+    kind="bar", ax=ax, color="dodgerblue", label="Train"
+)
+y_test.value_counts().plot(
+    kind="bar", ax=ax, color="royalblue", label="Test"
+)
+plt.legend()
+plt.show()
 
 
 # --------------------------------------------------------------
